d884ce4d3e8c97cc9892bd4fa81bb0d754e80952
gauravkhanna3007 <gaurav3007@gmail.com>
2024-03-19 01:46:31 +0530

    fdgdf

--- maintainsecret/save_to_repository.py
+++ maintainsecret/save_to_repository.py
@@ -18,7 +18,7 @@ import requests
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from maintainsecret.lib import (
+from lib import (
     GITHUB_DOT_COM_API_BASE,
     encrypt_secret,
     get_repository_public_key,
